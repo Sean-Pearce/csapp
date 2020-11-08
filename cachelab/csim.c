21d7289298705a86da59731ab59167bae490cdf0
Liyin Shao <shaoliyinde@gmail.com>
2020-11-08 13:37:51 +0800

    change code style

--- cachelab/csim.c
+++ cachelab/csim.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <stdio.h>
+
 #include "cachelab.h"
+
 #define false 0
 #define true 1
 
@@ -18,10 +20,8 @@ int main(int argc, char *argv[])
 
     char arg;
 
-    while ((arg = getopt(argc, argv, "vhs:E:b:t:")) != -1)
-    {
-        switch (arg)
-        {
+    while ((arg = getopt(argc, argv, "vhs:E:b:t:")) != -1) {
+        switch (arg) {
         case 'v':
             verbose = true;
             break;
@@ -46,34 +46,31 @@ int main(int argc, char *argv[])
     }
 
     // print help message
-    if (help)
-    {
+    if (help) {
         printf("Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n"
-               "Options:\n"
-               "  -h         Print this help message.\n"
-               "  -v         Optional verbose flag.\n"
-               "  -s <num>   Number of set index bits.\n"
-               "  -E <num>   Number of lines per set.\n"
-               "  -b <num>   Number of block offset bits.\n"
-               "  -t <file>  Trace file.\n\n"
-               "Examples:\n"
-               "  linux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.trace\n"
-               "  linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
+            "Options:\n"
+            "  -h         Print this help message.\n"
+            "  -v         Optional verbose flag.\n"
+            "  -s <num>   Number of set index bits.\n"
+            "  -E <num>   Number of lines per set.\n"
+            "  -b <num>   Number of block offset bits.\n"
+            "  -t <file>  Trace file.\n\n"
+            "Examples:\n"
+            "  linux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.trace\n"
+            "  linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
         exit(EXIT_SUCCESS);
     }
 
     // error checking
-    if (s < 0 || E < 0 || b < 0)
-    {
+    if (s < 0 || E < 0 || b < 0) {
         fprintf(stderr, "Invalid arguments.\n");
         exit(EXIT_FAILURE);
     }
 
-    if (verbose)
-    {
+    if (verbose) {
         printf("s = %d\nE = %d\nb = %d\nt = %s\n", s, E, b, t);
     }
-    
+
 
     printSummary(0, 0, 0);
     return 0;
