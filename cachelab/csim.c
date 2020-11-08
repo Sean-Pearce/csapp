72344e8148989cb1067976504010667b6ac960da
Liyin Shao <shaoliyinde@gmail.com>
2020-11-08 22:26:06 +0800

    handle trace file

--- cachelab/csim.c
+++ cachelab/csim.c
@@ -1,11 +1,26 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
 
 #include "cachelab.h"
 
 #define false 0
 #define true 1
+
+typedef struct {
+    CacheLine *head;
+    CacheLine *tail;
+} CacheSet;
+
+typedef struct {
+    CacheLine *prev;
+    CacheLine *next;
+    unsigned tag;
+    unsigned valid;
+} CacheLine;
 
 int main(int argc, char *argv[])
 {
@@ -62,16 +77,81 @@ int main(int argc, char *argv[])
     }
 
     // error checking
-    if (s < 0 || E < 0 || b < 0) {
+    if (s <= 0 || E <= 0 || b <= 0 || access(t, R_OK) == -1) {
         fprintf(stderr, "Invalid arguments.\n");
         exit(EXIT_FAILURE);
     }
 
-    if (verbose) {
-        printf("s = %d\nE = %d\nb = %d\nt = %s\n", s, E, b, t);
+    // init cache set
+    int S = pow(2, s);
+    CacheSet *cache = CreateCache(S, E);
+
+    // handle trace file
+    int hit, miss, evictions;
+    handle_trace(cache, s, b, S, E, t, &hit, &miss, &evictions);
+
+    printSummary(hit, miss, evictions);
+    return 0;
+}
+
+CacheSet *CreateCache(int S, int E)
+{
+    CacheSet *cache = (CacheSet *)calloc(S, sizeof(CacheSet));
+
+    for (int i = 0; i < S; i++) {
+        CacheLine *cachelines = (CacheLine *)calloc(E, sizeof(CacheLine));
+        CacheLine *prev = NULL;
+        for (int j = 0; j < E - 1; j++) {
+            cachelines[j].prev = prev;
+            cachelines[j].next = &cachelines[j + 1];
+            prev = &cachelines[j];
+        }
+        cachelines[E - 1].prev = prev;
     }
 
+    return cache;
+}
 
-    printSummary(0, 0, 0);
-    return 0;
+void handle_trace(CacheSet *cache, int s, int b, int S, int E, char *fname, int *hit, int *miss, int *evictions)
+{
+    char *line = NULL;
+    size_t len = 0;
+    FILE *fp = fopen(fname, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Open given trace file %s failed.\n", fname);
+        exit(EXIT_FAILURE);
+    }
+
+    char op;
+    unsigned long addr;
+    unsigned long tag;
+    unsigned long set;
+    const unsigned long mask = (1 << s) - 1;
+
+    while (getline(line, &len, fp) != -1) {
+        if (strlen(line) < 5 || line[0] != ' ') {
+            continue;
+        }
+
+        // parse trace
+        op = line[1];
+        addr = strtol(strsep(&line[3], ","), NULL, 16);
+        tag = addr >> (s + b);
+        set = (addr >> b) & mask;
+
+        printf("%c %x, %x", op, tag, set);
+
+        switch (op) {
+        case 'M':
+            break;
+        case 'L':
+            break;
+        case 'S':
+            break;
+        default:
+            continue;
+        }
+    }
+
+    close(fp);
 }
